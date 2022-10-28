e5b46e475739be6bbbb9f620d278024ef658c0b0
Felipe Risney <41243324+FRisney@users.noreply.github.com>
2022-10-27 22:37:49 -0300

    feature: Implementacao Remote Disciplina Repository

--- http_client/lib/disciplina/data/disciplina_repository.dart
+++ http_client/lib/disciplina/data/disciplina_repository.dart
@@ -5,20 +5,31 @@ class DisciplinaRemoteRepository implements DisciplinaRepository {
   RemoteClient client;
 
   @override
-  Future<Disciplina?> byId(int id) {
-    // TODO: implement byId
-    throw UnimplementedError();
+  Future<Disciplina?> byId(int id) async {
+    final response = await client.get('/api/v1/Disciplina/FindById/$id');
+    if (response.statusCode >= 300) {
+      throw RemoteClientException('Erro inesperado!');
+    }
+    if (response.statusCode == 203) return null;
+
+    return DisciplinaParser.fromMap(response.body);
   }
 
   @override
-  Future<List<Disciplina>> getAll() {
-    // TODO: implement getAll
-    throw UnimplementedError();
+  Future<List<Disciplina>> getAll() async {
+    final response = await client.get('/api/v1/Disciplina/FindAll');
+    final disciplinas = <Disciplina>[];
+    if (response.statusCode >= 300) {
+      throw RemoteClientException('Erro inesperado!');
+    }
+    if (response.statusCode != 203) {
+      for (var disciplina in (response.body['items'] as List)) {
+        disciplinas.add(DisciplinaParser.fromMap(disciplina));
+      }
+    }
+    return disciplinas;
   }
 
   @override
-  Future<List<Disciplina>> getMany(RepoFilter filter) {
-    // TODO: implement getMany
-    throw UnimplementedError();
-  }
+  Future<List<Disciplina>> getMany(RepoFilter filter) => getAll();
 }
