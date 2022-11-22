4b82303a4dae88e6efa9f72134c3ce2b17656185
Felipe Risney <41243324+FRisney@users.noreply.github.com>
2022-11-22 01:31:36 -0300

    Evitar saida acidental

--- app/lib/aluno/presentation/dashboard.dart
+++ app/lib/aluno/presentation/dashboard.dart
@@ -26,141 +26,182 @@ class DashboardScreen extends StatelessWidget {
       ),
       const SizedBox(width: 30),
     ];
-    return Scaffold(
-      drawer: Drawer(
-        child: SingleChildScrollView(
-          child: Container(
-            constraints: BoxConstraints(
-              maxHeight: MediaQuery.of(context).size.height,
-            ),
-            decoration: BoxDecoration(
-              gradient: UniUtiBgGradient3(),
-            ),
-            child: Column(
-              children: [
-                const DrawerHeader(
-                  child: UniUtiLogo(),
-                ),
-                const Expanded(child: Text('LOL')),
-                Column(
-                  children: const [
-                    Text('Sair'),
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-      body: CustomScrollView(
-        primary: true,
-        slivers: [
-          SliverAppBar(
-            backgroundColor: UniUtiColors.purple.shade900,
-            expandedHeight: 235,
-            floating: true,
-            snap: true,
-            pinned: true,
-            iconTheme: Theme.of(context)
-                .appBarTheme
-                .iconTheme!
-                .copyWith(color: Colors.white),
-            flexibleSpace: FlexibleSpaceBar(
-              collapseMode: CollapseMode.pin,
-              title: RichText(
-                text: TextSpan(
-                  style: th.bodyLarge!.copyWith(color: Colors.white),
-                  children: [
-                    const TextSpan(text: 'Bem-vindo, '),
-                    TextSpan(
-                        text: aluno.nome,
-                        style: th.bodyLarge!.copyWith(
-                            color: Colors.white, fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ),
-              background: Container(
-                padding: const EdgeInsets.only(top: 50),
-                decoration: BoxDecoration(gradient: UniUtiBgGradient2()),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(43),
-                        color: Colors.white,
-                      ),
-                      height: 85,
-                      width: 85,
-                      child: const Center(child: Text('FOTO')),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(aluno.nome,
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleLarge!
-                            .copyWith(color: Colors.white)),
-                    Text(aluno.instituicao?.nome ?? 'Sem Instituição',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium!
-                            .copyWith(color: Colors.white)),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              padding: const EdgeInsets.only(top: 30, bottom: 25),
+    return WillPopScope(
+      onWillPop: () async =>
+          (await showModalBottomSheet<bool>(
+            context: context,
+            builder: (context) => Padding(
+              // constraints: const BoxConstraints(maxHeight: 300),
+              padding: const EdgeInsets.fromLTRB(20, 35, 20, 15),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.only(left: 40, bottom: 10),
-                    child: Text(
-                      'Para onde deseja ir?',
-                      style: th.headlineSmall,
-                    ),
+                  Text(
+                    'Realmente deseja sair?',
+                    style: Theme.of(context).textTheme.headlineSmall,
                   ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(children: menuItems),
+                  const SizedBox(height: 18),
+                  Row(
+                    // mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: UniUtiSecondaryButton(
+                          onTap: () => Navigator.of(context).pop(true),
+                          title: 'Sair',
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: UniUtiPrimaryButton(
+                          onTap: () => Navigator.of(context).pop(false),
+                          title: 'Ficar',
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
-          ),
-          SliverToBoxAdapter(
+          )) ??
+          false,
+      child: Scaffold(
+        drawer: Drawer(
+          child: SingleChildScrollView(
             child: Container(
-              padding: const EdgeInsets.only(top: 30, bottom: 25),
-              child: Container(
-                padding: const EdgeInsets.only(left: 40, bottom: 10),
-                child: Text(
-                  'Ultimos itens vistos',
-                  style: th.headlineSmall,
-                ),
+              constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height,
+              ),
+              decoration: BoxDecoration(
+                gradient: UniUtiBgGradient3(),
+              ),
+              child: Column(
+                children: [
+                  const DrawerHeader(
+                    child: UniUtiLogo(),
+                  ),
+                  const Expanded(child: Text('LOL')),
+                  Column(
+                    children: const [
+                      Text('Sair'),
+                    ],
+                  )
+                ],
               ),
             ),
           ),
-          FutureBuilder<List<RecentsListItem>>(
-            future: controller.getRecentes(),
-            builder: (context, snap) {
-              if (snap.connectionState == ConnectionState.done) {
-                if (snap.hasData && snap.data!.isNotEmpty) {
-                  return SliverFixedExtentList(
-                    itemExtent: 105,
-                    delegate: SliverChildListDelegate(snap.data!),
-                  );
+        ),
+        body: CustomScrollView(
+          primary: true,
+          slivers: [
+            SliverAppBar(
+              backgroundColor: UniUtiColors.purple.shade900,
+              expandedHeight: 235,
+              floating: true,
+              snap: true,
+              pinned: true,
+              iconTheme: Theme.of(context)
+                  .appBarTheme
+                  .iconTheme!
+                  .copyWith(color: Colors.white),
+              flexibleSpace: FlexibleSpaceBar(
+                collapseMode: CollapseMode.pin,
+                title: RichText(
+                  text: TextSpan(
+                    style: th.bodyLarge!.copyWith(color: Colors.white),
+                    children: [
+                      const TextSpan(text: 'Bem-vindo, '),
+                      TextSpan(
+                          text: aluno.nome,
+                          style: th.bodyLarge!.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                background: Container(
+                  padding: const EdgeInsets.only(top: 50),
+                  decoration: BoxDecoration(gradient: UniUtiBgGradient2()),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(43),
+                          color: Colors.white,
+                        ),
+                        height: 85,
+                        width: 85,
+                        child: const Center(child: Text('FOTO')),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(aluno.nome,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(color: Colors.white)),
+                      Text(aluno.instituicao?.nome ?? 'Sem Instituição',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium!
+                              .copyWith(color: Colors.white)),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                padding: const EdgeInsets.only(top: 30, bottom: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 40, bottom: 10),
+                      child: Text(
+                        'Para onde deseja ir?',
+                        style: th.headlineSmall,
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(children: menuItems),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                padding: const EdgeInsets.only(top: 30, bottom: 25),
+                child: Container(
+                  padding: const EdgeInsets.only(left: 40, bottom: 10),
+                  child: Text(
+                    'Ultimos itens vistos',
+                    style: th.headlineSmall,
+                  ),
+                ),
+              ),
+            ),
+            FutureBuilder<List<RecentsListItem>>(
+              future: controller.getRecentes(),
+              builder: (context, snap) {
+                if (snap.connectionState == ConnectionState.done) {
+                  if (snap.hasData && snap.data!.isNotEmpty) {
+                    return SliverFixedExtentList(
+                      itemExtent: 105,
+                      delegate: SliverChildListDelegate(snap.data!),
+                    );
+                  }
+                  return const SliverToBoxAdapter(
+                      child: Center(child: Text('Sem Monitorias')));
                 }
                 return const SliverToBoxAdapter(
-                    child: Center(child: Text('Sem Monitorias')));
-              }
-              return const SliverToBoxAdapter(
-                  child: Center(child: CircularProgressIndicator()));
-            },
-          ),
-        ],
+                    child: Center(child: CircularProgressIndicator()));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
